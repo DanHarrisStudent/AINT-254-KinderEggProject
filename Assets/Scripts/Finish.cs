2b0ba4574ee617953673ee0bfb8366c628083a66
DanHarrisStudent <32270584+DanHarrisStudent@users.noreply.github.com>
2017-11-02 15:35:58 +0000

    Work for Demo

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour {
+
+    public GameObject _trigger;
+    public GameObject _UI;
+    public float _menuTime = 2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(_trigger);
+        Time.timeScale = 1f;
+    }
+
+}
