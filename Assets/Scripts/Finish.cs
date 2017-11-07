b58f3e4a2b45205c7bccb1e4103f0e07e7081779
DanHarrisStudent <32270584+DanHarrisStudent@users.noreply.github.com>
2017-11-07 13:21:58 +0000

    Revert "Demo Session Set up"
    This reverts commit 94c28ae5ddeba02d90db71b8b7bfd98a86ef96fd.

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class Finish : MonoBehaviour {
 
     public GameObject _trigger;
-    public GameObject _menu;
+    public GameObject _UI;
+    public float _menuTime = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
-        _menu.SetActive(true);
-
         Destroy(_trigger);
-    }
-    private void OpenMenu()
-    {
-        if (Input.GetButtonDown("Start"))
-        {
-            _menu.SetActive(true);
-        }
+        Time.timeScale = 1f;
     }
 
 }
